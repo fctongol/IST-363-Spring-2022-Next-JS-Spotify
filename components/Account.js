c188f25f8b2242bad8cf43f19f6605814fbd3626
fctongol <74747172+fctongol@users.noreply.github.com>
2022-05-13 01:14:26 -0400

    first commit

---
+++ components/Account.js
@@ -0,0 +1,11 @@
+import Layout from "./Layout"
+import Heading from "./Heading"
+
+const AccountPage = () => {
+    return <Layout>
+        <Heading level="1">
+            Account
+        </Heading>
+    </Layout>
+}
+export default AccountPage;
---
+++ pages/tracks/index.js
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+
+import Heading from '../../components/Heading'
+import Layout from '../../components/Layout'
+
+import { getTracks } from '../../lib/api'
+
+export async function getStaticProps () {
+    //Get external data from the file system, API, DB, etc..
+    const tracks = getTracks();
+
+    // The value of the `Props` key will be
+    // Passed to the `Home` component
+    return {
+        props: {
+            tracks
+        }
+    }
+}
+
+const TracksLandingPage = ({ tracks }) => {
+    return <Layout>
+        <Heading level="1">All tracks</Heading>
+        {tracks.map((track, index) => {
+            const { title, slug } = track;
+            return <p key={index}>
+                <Link href={`/tracks/${track.slug}`}>
+                    <a>
+                    {title}
+                    </a>
+                </Link></p>
+        })}
+    </Layout>
+}
+export default TracksLandingPage;
